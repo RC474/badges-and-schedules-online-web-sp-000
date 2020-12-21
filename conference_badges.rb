8352163a7810d4035217a271e1d7c7ac66a81210
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-12-21 19:21:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.each do |name|
+    badge_maker(name)
+  end
+end
